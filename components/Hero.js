e6c788331d608b79931217535ebe7c1164122c51
valentinpain <44436240+valentinpain@users.noreply.github.com>
2023-06-07 00:40:17 +0200

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ export default function Hero({title,heroUrl}){
             <img src={heroUrl} className={"object-cover min-h-full max-h-full min-w-full"}/>
             <div className={"absolute top-0 h-full w-full flex flex-col items-center justify-center bg-black bg-opacity-30 gap-y-4"}>
                 <h1 className={"text-white font-outfit text-4xl md:text-6xl lg:text-9xl"}>{title}</h1>
-                <button className={"btn_primary"}>Mon bouton</button>
+                <a href="#presentation"><button className={"btn_primary"}>Présentation</button></a>
             </div>
         </section>
     );
