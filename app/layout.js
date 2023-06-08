b56ea0ff6fcdba2ce6e2d5bc3166c4e76cd6dd74
valentinpain <44436240+valentinpain@users.noreply.github.com>
2023-06-08 13:13:30 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 
   return (
-    <html lang="fr">
+    <html lang="fr" className="scroll-smooth">
       <body>
             <Layout>
                 {children}
