a4fc063072ba8119dd73db9d9693647210cab2c8
valentinpain <44436240+valentinpain@users.noreply.github.com>
2023-06-08 18:26:47 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,9 @@ import Layout from "@/components/Layout";
 export const metadata = {
   title: 'GreenShift',
   description: '',
+  icons : {
+            icon : ${process.env.DIRECTUS_FRONT_URL_DOCUMENT}${seo.favicon},
+        },
 }
 
 export default function RootLayout({ children }) {
