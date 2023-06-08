a5bab03a7ee98e2bdc3d05e68e4709076cd550f6
valentinpain <44436240+valentinpain@users.noreply.github.com>
2023-06-08 13:09:11 +0200

    Update programList.js

--- components/programList.js
+++ components/programList.js
@@ -5,7 +5,7 @@ export default function ProgramList(){
 
     return(
         <section className="flex flex-col items-center w-full mt-2 font-outfit m-auto">
-            <h1 className="text-center text-5xl mb-2">Nos programmes</h1>
+            <h1 className="text-center md:text-5xl text-3xl mb-2">Nos programmes</h1>
             <p className="text-center text-gray-500 mb-10">Phrase introduction</p>
 
             <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6 md:px-28 px-10">
